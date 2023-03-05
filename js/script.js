986e168838483efd0b873fd78c0657c19ee0d8c8
Josh Hammond <95947893+jwhammond20@users.noreply.github.com>
2023-03-05 18:59:18 -0500

    api connected and finished responsive layout

--- js/script.js
+++ js/script.js
@@ -1,13 +1,67 @@
-let videoList = document.querySelectorAll('.video-list-container .list');
+const apiKey = "AIzaSyDiRqzcOWvgk5XTdtpi-tWq0CYLp_ZONZo";
+const playlistId = "PL-nKWS2MEWvoSBGGuGSclXwpHzSERR6bK";
 
-videoList.forEach(vid =>{
-   vid.onclick = () =>{
-      videoList.forEach(remove =>{remove.classList.remove('active')});
-      vid.classList.add('active');
-      let src = vid.querySelector('.list-video').src;
-      let title = vid.querySelector('.list-title').innerHTML;
-      document.querySelector('.main-video-container .main-video').src = src;
-      document.querySelector('.main-video-container .main-video').play();
-      document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
-   };
-});
+const apiUrl = "https://www.googleapis.com/youtube/v3/playlistItems";
+const queryParams = `?part=snippet&maxResults=10&playlistId=${playlistId}&key=${apiKey}`;
+
+fetch(apiUrl + queryParams)
+  .then((response) => response.json())
+  .then((data) => {
+    const videoArray = data.items
+      .map((item) => {
+        const feedTitle = item.snippet.title;
+        /* const feedDesc = item.snippet.description; */
+        const videoID = item.snippet.resourceId.videoId;
+        if (
+          feedTitle.toLowerCase() !== "private video" &&
+          feedTitle.toLowerCase() !== "deleted video"
+        ) {
+          return {
+            title: item.snippet.title,
+            thumbnail: "https://img.youtube.com/vi/" + videoID + "/0.jpg",
+            url: `https://www.youtube.com/embed/${videoID}`,
+          };
+        } else {
+          return null;
+        }
+      })
+      .filter((video) => video !== null);
+
+    const videoList = document.getElementById("video-list");
+    videoArray.forEach((video, index) => {
+      const isActive = index === 0 ? "active" : "";
+      const videoElement = document.createElement("div");
+      videoElement.innerHTML = `
+          <div class="list ${isActive}">
+            <img class="list-thumbnail" src="${video.thumbnail}" alt="${video.title}">
+            <div class="list-title">${video.title}</div>
+          </div>
+        `;
+      videoList.appendChild(videoElement);
+
+      const listItem = videoElement.querySelector(".list");
+      listItem.onclick = (event) => {
+        event.preventDefault();
+        videoList.querySelectorAll(".list").forEach((remove) => {
+          remove.classList.remove("active");
+        });
+        listItem.classList.add("active");
+        document.querySelector(".main-video-container .main-video").src =
+          video.url;
+        document.querySelector(
+          ".main-video-container .main-vid-title"
+        ).innerHTML = `<h4 class="main-vid-title">${video.title}</h4>`;
+      };
+    });
+
+    // Set main video properties based on first video
+    const firstVideo = videoArray[0];
+    document.querySelector(".main-video-container .main-video").src =
+      firstVideo.url;
+    document.querySelector(
+      ".main-video-container .main-vid-title"
+    ).innerHTML = `<h4 class="main-vid-title">${firstVideo.title}</h4>`;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
